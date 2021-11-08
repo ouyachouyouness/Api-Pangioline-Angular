16ed69ffdb508f7d8eedf1a2b36c9164d77c6568
OUYACHOU Younes <ouyachouyouness@gmail.com>
2021-11-08 11:12:15 +0100

    Add components services models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddAddressComponent } from './components/add-address/add-address.component';
+import { EditAddressComponent } from './components/edit-address/edit-address.component';
+import { ListAddressComponent } from './components/list-address/list-address.component';
+import { NavbarComponent } from './components/partials/navbar/navbar.component';
+import { PageNotFoundComponent } from './components/partials/page-not-found/page-not-found.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddAddressComponent,
+    EditAddressComponent,
+    ListAddressComponent,
+    NavbarComponent,
+    PageNotFoundComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
