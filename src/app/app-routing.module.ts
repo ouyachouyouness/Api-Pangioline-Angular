1d3cab23ae6823a1402dbc359a1685de6a841a47
OUYACHOU Younes <ouyachouyouness@gmail.com>
2021-11-08 13:41:40 +0100

    Nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddAddressComponent } from './components/add-address/add-address.component';
+import { EditAddressComponent } from './components/edit-address/edit-address.component';
+import { ListAddressComponent } from './components/list-address/list-address.component';
+import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/partials/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"", redirectTo:'/adress', pathMatch: 'full'
+  },
+  {
+    path: "adress", children:[
+      {
+        path:"", component: ListAddressComponent
+      },
+      {
+        path:"create", component: AddAddressComponent
+      },
+      {
+        path:"edit/:id", component: EditAddressComponent
+      },
+    ]
+  },
+  {
+    path:"login", component: LoginComponent
+  },
+  {
+    path:"**", component: PageNotFoundComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
